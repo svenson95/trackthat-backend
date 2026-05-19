4f42b22263a5223a9a7c3d7f148219c7e6f1e7bd
Sven Brodny <svenbrodny@googlemail.com>
2026-05-20 00:51:38 +0200

    fix(health): missing annotation & controller path

--- src/main/java/com/svenson95/track_e_backend/health/controller/HealthController.java
+++ src/main/java/com/svenson95/track_e_backend/health/controller/HealthController.java
@@ -2,7 +2,11 @@ package com.svenson95.track_e_backend.health.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api")
 public class HealthController {
   @GetMapping("/health")
   public ResponseEntity<Void> health() {
